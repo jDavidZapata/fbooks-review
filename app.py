193fb309f0eb6061d185bab8b69af308c4840b74
Juan David Zapata <jdavidzapata1983@gmail.com>
2018-12-02 11:09:59 -0500

    Update

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ if not os.getenv("DATABASE_URL"):
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-#Session(app)
+# Session(app)
 app.secret_key = b'_3#y2L"F4Q8z\n\xec]/'
 
 # Set up database
@@ -24,18 +24,17 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
-
 @app.route("/", methods=["GET", "POST"])
 def index():
-    
+
     error = None
 
     book = None
-    
-    books = db.execute('SELECT * FROM books ORDER BY random() LIMIT 9').fetchall()
+
+    books = db.execute(
+        'SELECT * FROM books ORDER BY random() LIMIT 9').fetchall()
 
     db.close()
-
 
     """Check to see if User is in session."""
 
@@ -46,13 +45,12 @@ def index():
 
         return render_template("index.html", books=books, error=error, book=book)
     else:
-        g.user = db.execute(    
-            'SELECT * FROM users WHERE id = :id', {"id": user_id,}
+        g.user = db.execute(
+            'SELECT * FROM users WHERE id IN (:id)', {"id": user_id, }
         ).fetchone()
         db.close()
 
         return render_template("index.html", books=books, error=error, book=book)
-        
 
 
 @app.route('/register', methods=('GET', 'POST'))
@@ -71,105 +69,107 @@ def register():
 
     if ('user_id' in session):
 
-        g.user = db.execute(    
-            'SELECT * FROM users WHERE id = :id', {"id": user_id,}
+        g.user = db.execute(
+            'SELECT * FROM users WHERE id IN (:id)', {"id": user_id, }
         ).fetchone()
 
         db.close()
 
         # change to redirect to current page
         return render_template("index.html")
-         
 
     if request.method == 'POST':
 
         name = request.form['name']
         password = request.form['password']
         email = request.form['email']
-        
+
         error = None
 
         if not name:
             error = 'Name is required.'
         elif not password:
             error = 'Password is required.'
-        
+        elif not email:
+            error = 'Email required.'
 
-        user = db.execute("SELECT * FROM users WHERE name = :name AND email = :email", {"name": name, "email": email}).fetchone()
-        
+        user = db.execute("SELECT * FROM users WHERE email IN (:email)",
+                          {"email": email}).fetchone()
 
         if user is not None:
-            error = 'User {0} is already registered.'.format(name)
+            error = 'User with email {0} is already registered.'.format(email)
 
 
         if error is None:
             """If the name is available, store it in the database and go to the login page"""
 
             db.execute("INSERT INTO users (name, password, email) VALUES (:name, :password, :email)",
-                   {"name": name, "password": password, "email": email})
+                       {"name": name, "password": password, "email": email})
             db.commit()
+
             
-            user = db.execute("SELECT * FROM users WHERE name = :name AND email = :email", {"name": name, "email": email}).fetchone()
+            user = db.execute("SELECT * FROM users WHERE email IN (:email)", {
+                              "email": email}).fetchone()
+            
 
             """Store the user id in a new session and return to the index"""
-            
+
             session.clear()
             session['user_id'] = user['id']
-            g.user = db.execute(    
-            'SELECT * FROM users WHERE id = :id', {"id": user_id,}
-            ).fetchone()
-            db.close()
+            g.user = user 
             
+            '''
+            g.user = db.execute(
+                'SELECT * FROM users WHERE id IN (:id)', {"id": user_id, }
+            ).fetchone()
+            '''
+
+            db.close()
+
             success = 'Thank You For Signing Up.'
 
             # Change to redirect to curent page
             return redirect(url_for('index'))
-        
 
     return render_template('auth/register.html', book=book, error=error, success=success)
-
-
-
-
 
 
 @app.route('/login', methods=('GET', 'POST'))
 def login():
     """Log in a registered user by adding the user id to the session."""
-    
-    error = None 
+
+    error = None
 
     success = None
 
     user_id = session.get('user_id')
     if ('user_id' in session):
-        g.user = db.execute(    
-            'SELECT * FROM users WHERE id = :id', {"id": user_id,}
+        g.user = db.execute(
+            'SELECT * FROM users WHERE id IN (:id)', {"id": user_id, }
         ).fetchone()
         db.close()
 
         # change to redirect to current page
         return render_template("index.html", error=error)
-    
-    
-    if request.method == 'POST':
-        name = request.form['name']
-        password = request.form['password']
-        
-        error = None
-        user = db.execute("SELECT * FROM users WHERE name = :name AND password = :password", {"name": name, "password": password}).fetchone()
 
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        error = None
+        user = db.execute("SELECT * FROM users WHERE email IN (:email) AND password IN (:password)",
+                          {"email": email, "password": password}).fetchone()
 
         if user is None:
-            error = 'Incorrect Name or Password.'
-        
+            error = 'Incorrect Email or Password.'
 
         if error is None:
+
             """Store the user id in a new session and return to the index."""
             session.clear()
             session['user_id'] = user['id']
-            g.user = db.execute(    
-            'SELECT * FROM users WHERE id = :id', {"id": user_id,}
+            g.user = db.execute(
+                'SELECT * FROM users WHERE id IN (:id)', {"id": user_id, }
             ).fetchone()
             db.close()
 
@@ -177,12 +177,7 @@ def login():
 
             return redirect(url_for('index'))
 
-        
-        
     return render_template('auth/login.html', book=book, error=error, success=success)
-    
-
-
 
 
 @app.route('/logout')
@@ -199,7 +194,6 @@ def logout():
     return redirect(url_for('index', success=success))
 
 
-
 @app.route('/search', methods=['GET', 'POST'])
 def search():
 
@@ -208,7 +202,7 @@ def search():
     book = None
 
     books = None
-    
+
     user_id = session.get('user_id')
 
     """Check to see if User is in session."""
@@ -217,54 +211,56 @@ def search():
         g.user = None
 
     else:
-        g.user = db.execute(    
-                'SELECT * FROM users WHERE id = :id', {"id": user_id,}
-            ).fetchone()
+        g.user = db.execute(
+            'SELECT * FROM users WHERE id IN (:id)', {"id": user_id, }
+        ).fetchone()
 
-        db.close()     
-    
+        db.close()
 
     if request.method == 'POST':
-                
+
         if request.form.get('b_title', None):
             b_title = request.form['b_title']
-            books = db.execute("SELECT * FROM books WHERE title ILIKE ('%' || :title || '%')", {"title": b_title,}).fetchall()
+            books = db.execute(
+                "SELECT * FROM books WHERE title ILIKE ('%' || :title || '%')", {"title": b_title, }).fetchall()
             db.close()
             if books is None:
                 error = "No Such Title"
-            return render_template("search.html", books=books ,error=error, book=book)
-            
+            return render_template("search.html", books=books, error=error, book=book)
+
         elif request.form.get('b_author', None):
             b_author = request.form['b_author']
-            books = db.execute("SELECT * FROM books WHERE author ILIKE ('%' || :author || '%')", {"author": b_author,}).fetchall()
+            books = db.execute(
+                "SELECT * FROM books WHERE author ILIKE ('%' || :author || '%')", {"author": b_author, }).fetchall()
             db.close()
             if books is None:
                 error = "No Such Author"
-            return render_template("search.html", books=books ,error=error, book=book)
+            return render_template("search.html", books=books, error=error, book=book)
 
         elif request.form.get('b_isbn', None):
             b_isbn = request.form['b_isbn']
-            books = db.execute("SELECT * FROM books WHERE isbn ILIKE ('%' || :isbn || '%')", {"isbn": b_isbn,}).fetchall()
+            books = db.execute(
+                "SELECT * FROM books WHERE isbn ILIKE ('%' || :isbn || '%')", {"isbn": b_isbn, }).fetchall()
             db.close()
             if books is None:
                 error = "No Such isbn #"
-            return render_template("search.html", books=books ,error=error, book=book)
+            return render_template("search.html", books=books, error=error, book=book)
 
         else:
-            books = db.execute('SELECT * FROM books ORDER BY random() LIMIT 9').fetchall()
+            books = db.execute(
+                'SELECT * FROM books ORDER BY random() LIMIT 9').fetchall()
             db.close()
 
-            return render_template("search.html", books=books ,error=error)
+            return render_template("search.html", books=books, error=error)
 
     # book = db.execute('SELECT * FROM books WHERE title = :title', {"title": b_title,}).fetchone()
-    books = db.execute('SELECT * FROM books ORDER BY random() LIMIT 9').fetchall()
+    books = db.execute(
+        'SELECT * FROM books ORDER BY random() LIMIT 9').fetchall()
 
     return render_template("search.html", books=books, error=error, book=book)
 
 
-
-
-@app.route('/book/<string:b_title>' )
+@app.route('/book/<string:b_title>')
 def book(b_title):
 
     error = None
@@ -276,31 +272,31 @@ def book(b_title):
     if (not 'user_id' in session):
         g.user = None
 
-        
         #book = db.execute('SELECT * FROM books WHERE title = :title', {"title": b_title,}).fetchone()
-        book = db.execute('SELECT title, author, isbn, year, round(avg(rating), 1), count(rating) from books left join reviews on isbn = rbook_isbn where title = :title group by title, author, isbn, year', {"title": b_title,}).fetchone()
-    
+        book = db.execute('SELECT title, author, isbn, year, round(avg(rating), 1), count(rating) from books left join reviews on isbn = rbook_isbn where title IN (:title) group by title, author, isbn, year', {
+                          "title": b_title, }).fetchone()
+
         return render_template("bookpage.html", book=book, error=error)
 
     else:
-        g.user = db.execute(    
-            'SELECT * FROM users WHERE id = :id', {"id": user_id,}
+        g.user = db.execute(
+            'SELECT * FROM users WHERE id IN (:id)', {"id": user_id, }
         ).fetchone()
 
         db.close()
-        
-        # book = db.execute('SELECT * FROM books WHERE title = :title', {"title": b_title,}).fetchone() 
-        book = db.execute('SELECT title, author, isbn, year, round(avg(rating), 1), count(rating) from books left join reviews on isbn = rbook_isbn where title = :title group by title, author, isbn, year', {"title": b_title,}).fetchone()
+
+        # book = db.execute('SELECT * FROM books WHERE title = :title', {"title": b_title,}).fetchone()
+        book = db.execute('SELECT title, author, isbn, year, round(avg(rating), 1), count(rating) from books left join reviews on isbn = rbook_isbn where title IN (:title) group by title, author, isbn, year', {
+                          "title": b_title, }).fetchone()
 
         session['b_title'] = b_title
 
         return render_template("bookpage.html", book=book, error=error)
 
 
-
 @app.route('/create', methods=['GET', 'POST'])
 def create():
-    
+
     error = None
 
     success = None
@@ -311,24 +307,24 @@ def create():
 
     if (not 'user_id' in session):
         g.user = None
-        
 
         return render_template("bookpage.html", error=error, book=book)
-    
 
     else:
 
         user_id = session.get('user_id')
-        
+
         b_title = session.get('b_title')
 
-        # book = db.execute('SELECT * FROM books WHERE title = :title', {"title": b_title,}).fetchone() 
-        book = db.execute('SELECT title, author, isbn, year, round(avg(rating), 1), count(rating) from books left join reviews on isbn = rbook_isbn where title = :title group by title, author, isbn, year', {"title": b_title,}).fetchone()
+        # book = db.execute('SELECT * FROM books WHERE title = :title', {"title": b_title,}).fetchone()
+        book = db.execute('SELECT title, author, isbn, year, round(avg(rating), 1), count(rating) from books left join reviews on isbn = rbook_isbn where title IN (:title) group by title, author, isbn, year', {
+                          "title": b_title, }).fetchone()
 
-        g.user = db.execute('SELECT * FROM users WHERE id = :id', {"id": user_id,}).fetchone()
+        g.user = db.execute(
+            'SELECT * FROM users WHERE id IN (:id)', {"id": user_id, }).fetchone()
 
         db.close()
-    
+
         if request.method == 'POST':
 
             rating = request.form['radio']
@@ -339,43 +335,50 @@ def create():
             error = None
 
             # check to see if user id is in the review for that book
-                
+
             if error is None:
 
-                user_review = db.execute("SELECT * FROM reviews WHERE review_user_id = :review_user_id AND rbook_isbn = :rbook_isbn", {"review_user_id": review_user_id, "rbook_isbn": rbook_isbn}).fetchone()
-            
+                user_review = db.execute("SELECT * FROM reviews WHERE review_user_id IN (:review_user_id) AND rbook_isbn IN (:rbook_isbn)", {
+                                         "review_user_id": review_user_id, "rbook_isbn": rbook_isbn}).fetchone()
 
                 if user_review is not None:
 
                     error = 'You have already Reviewed this Book.'
 
-                
-                else:    
+                else:
 
                     db.execute("INSERT INTO reviews (rating, review_text, review_user_id, rbook_isbn) VALUES (:rating, :review_text, :review_user_id, :rbook_isbn)",
-                            {"rating": rating, "review_text": review_text, "review_user_id": review_user_id, "rbook_isbn": rbook_isbn})
+                               {"rating": rating, "review_text": review_text, "review_user_id": review_user_id, "rbook_isbn": rbook_isbn})
                     db.commit()
 
                     success = 'Great Review!!!'
 
-                    book = db.execute('SELECT title, author, isbn, year, round(avg(rating), 1), count(rating) from books left join reviews on isbn = rbook_isbn where title = :title group by title, author, isbn, year', {"title": b_title,}).fetchone()
+                    book = db.execute('SELECT title, author, isbn, year, round(avg(rating), 1), count(rating) from books left join reviews on isbn = rbook_isbn where title IN (:title) group by title, author, isbn, year', {
+                                      "title": b_title, }).fetchone()
 
-    
-                    return render_template("bookpage.html", error=error, book=book, success=success)       
-        
-    
-    
+                    return render_template("bookpage.html", error=error, book=book, success=success)
 
         return render_template("bookpage.html", error=error, book=book)
-   
-    
 
-    
 
-    
- 
- 
+@app.route('/api/<isbn>', methods=['GET', 'POST'])
+def api(isbn):
+    if request.method == 'GET':
+
+        '''
+        make jason response
+
+        {
+        "title": "Memory",
+        "author": "Doug Lloyd",
+        "year": 2015,
+        "isbn": "1632168146",
+        "review_count": 28,
+        "average_score": 5.0
+        }
+        '''
+
+
 if __name__ == "__main__":
     app.secret_key = os.urandom(13)
     app.run()
-
--- import.py
+++ import.py
@@ -8,12 +8,13 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
+
 def main():
     db.execute("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name VARCHAR NOT NULL, password VARCHAR NOT NULL, email VARCHAR unique)")
     db.execute("CREATE TABLE IF NOT EXISTS books (id SERIAL PRIMARY KEY, isbn VARCHAR NOT NULL, title VARCHAR NOT NULL, author VARCHAR NOT NULL, year INTEGER NOT NULL)")
-    db.execute("CREATE TABLE IF NOT EXISTS reviews (id SERIAL PRIMARY KEY, rating INTEGER NOT NULL, review_text VARCHAR NOT NULL, review_user_id INTEGER REFERENCES users(id), rbook_isbn VARCHAR REFERENCES books(isbn))")
+    db.execute("CREATE TABLE IF NOT EXISTS reviews (id SERIAL PRIMARY KEY, rating INTEGER NOT NULL, review_text VARCHAR NOT NULL, review_user_id NOT NULL INTEGER REFERENCES users(id), rbook_isbn VARCHAR NOT NULL REFERENCES books(isbn))")
 
-    # Opens the file   
+    # Opens the file
     f = open("books.csv")
 
     # Creates iterator to read the file line by line
@@ -26,10 +27,11 @@ def main():
     for isbn, title, author, year in reader:
         db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
                    {"isbn": isbn, "title": title, "author": author, "year": year})
-        print(f"Added table books with isbn # {isbn} called {title} writen by {author} on the year {year}.")
-   
+        print(
+            f"Added table books with isbn # {isbn} called {title} writen by {author} on the year {year}.")
+
     db.commit()
-    
+
 
 if __name__ == "__main__":
     main()
