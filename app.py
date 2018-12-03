dbee192d188528bbe0d98ad4e21ba5a72974c37b
Juan David Zapata <jdavidzapata1983@gmail.com>
2018-12-03 18:15:03 -0500

    Update

--- app.py
+++ app.py
@@ -279,7 +279,7 @@ def book(b_title):
         
         b_isbn = book['isbn']
 
-        res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "KEY", "isbns": b_isbn})
+        res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "Q34myL0Dt3PWGCU73EGqJA", "isbns": b_isbn})
         
         if res.status_code != 200:
             raise Exception("ERROR: API request unsuccessful.")
