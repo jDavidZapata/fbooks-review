39d3617fc86b2bc597f321682b5f35d01f68e087
Juan David Zapata <jdavidzapata1983@gmail.com>
2019-01-31 21:52:07 -0500

    Updated import.py

--- import.py
+++ import.py
@@ -1,37 +1,34 @@
 import csv
 import os
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+from flask import Flask, render_template, request
+from models import *
+
 
 # Creates connection to DataBase and Binds to db
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
-
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
 
 def main():
-    db.execute("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name VARCHAR NOT NULL, password VARCHAR NOT NULL, email VARCHAR unique)")
-    db.execute("CREATE TABLE IF NOT EXISTS books (id SERIAL PRIMARY KEY, isbn VARCHAR NOT NULL unique, title VARCHAR NOT NULL, author VARCHAR NOT NULL, year INTEGER NOT NULL)")
-    db.execute("CREATE TABLE IF NOT EXISTS reviews (id SERIAL PRIMARY KEY, rating INTEGER NOT NULL, review_text VARCHAR NOT NULL, review_user_id NOT NULL INTEGER REFERENCES users(id), rbook_isbn VARCHAR NOT NULL REFERENCES books(isbn))")
 
     # Opens the file
-    f = open("books.csv")
+    b = open("books.csv")
 
     # Creates iterator to read the file line by line
-    reader = csv.reader(f)
+    reader = csv.reader(b)
 
     # Skips first line of CSV file
     next(reader, None)
 
     # Inserts values in to DataBase on line at a time
     for isbn, title, author, year in reader:
-        db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
-                   {"isbn": isbn, "title": title, "author": author, "year": year})
-        print(
-            f"Added table books with isbn # {isbn} called {title} writen by {author} on the year {year}.")
-
-    db.commit()
-
+        book = Book(isbn=isbn, title=title, author=author, year=year)
+        db.session.add(book)
+        print(f"Added book: {title} by {author} published on {year}.")
+    db.session.commit()
 
 if __name__ == "__main__":
-    main()
+    with app.app_context():
+        main()
